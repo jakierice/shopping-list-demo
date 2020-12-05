e14a0dd1b4d6af2cb87e9075af9ad3d477cd53e5
Jakie W. Rice <jakie.rice@gmail.com>
2020-12-05 10:00:02 -0600

    refactor: create generic useForm hook and reimplemnent new item form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react"
-import * as R from "ramda"
-import { v4 as uuid } from "uuid"
+import React from "react";
+import * as R from "ramda";
+import { v4 as uuid } from "uuid";
 
-import "./App.css"
+import "./App.css";
 
-const useShoppingListState = (initialState) => {
-  const [list, setList] = React.useState(initialState)
+function useShoppingListState(initialState) {
+  const [list, setList] = React.useState(initialState);
 
   return {
     setListThunks: {
@@ -19,80 +19,82 @@ const useShoppingListState = (initialState) => {
 
       clear: () => setList([]),
     },
+
     shoppingList: list,
-  }
+  };
 }
 
-const useShoppingListItemForm = ({ onSubmit }) => {
-  const [name, setName] = React.useState("")
-  const [quantity, setQuantity] = React.useState(0)
-  const isValid = R.allPass([
-    R.propSatisfies(R.pipe(R.isEmpty, R.not), "name"),
-    R.propSatisfies(R.lt(0), "quantity"),
-  ])({ name, quantity })
+function useForm({ onSubmit, initialValues, validations }) {
+  const [values, setValues] = React.useState(initialValues);
+  const isValid = R.allPass(validations)(values);
 
   const resetForm = () => {
-    setName("")
-    setQuantity(0)
-  }
+    setValues(initialValues);
+  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  function handleSubmit(event) {
+    event.preventDefault();
 
     if (isValid) {
-      onSubmit({ name, quantity })()
-      resetForm()
+      onSubmit(values)();
+      resetForm();
     }
   }
 
   return {
-    fields: {
-      name: {
-        value: name,
-        handler: (e) => setName(e.target.value),
-      },
-      quantity: {
-        value: quantity,
-        handler: (e) => setQuantity(parseInt(e.target.value)),
-      },
-    },
+    values,
     isValid,
     handleSubmit,
-  }
+    handleOnChange: (field) => (e) =>
+      setValues(R.set(R.lensProp(field), e.target.value)),
+  };
+}
+
+function NewShoppingListItemForm({ handleNewItemCreation }) {
+  const validations = [
+    R.propSatisfies(R.compose(R.not, R.isEmpty), "name"),
+    R.propSatisfies(R.lt(0), "quantity"),
+  ];
+
+  const { isValid, handleSubmit, handleOnChange, values } = useForm({
+    onSubmit: handleNewItemCreation,
+    initialValues: { name: "", quantity: 0 },
+    validations,
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Add New Item</h3>
+      <label>
+        Name
+        <input
+          type="text"
+          value={values.name}
+          onChange={handleOnChange("name")}
+        />
+      </label>
+      <label>
+        Quantity
+        <input
+          type="number"
+          value={values.quantity}
+          onChange={handleOnChange("quantity")}
+        />
+      </label>
+      <button onClick={handleSubmit} disabled={!isValid}>
+        Add Item
+      </button>
+    </form>
+  );
 }
 
 function ShoppingList(props) {
-  const { setListThunks, shoppingList } = useShoppingListState([])
-
-  const { fields, isValid, handleSubmit } = useShoppingListItemForm({
-    onSubmit: setListThunks.newItem,
-  })
+  const { setListThunks, shoppingList } = useShoppingListState([]);
 
   return (
     <div>
       <h2>{props.listName}</h2>
-      <form onSubmit={handleSubmit}>
-        <h3>Add New Item</h3>
-        <label>
-          Name
-          <input
-            type="text"
-            value={fields.name.value}
-            onChange={fields.name.handler}
-          />
-        </label>
-        <label>
-          Quantity
-          <input
-            type="number"
-            value={fields.quantity.value}
-            onChange={fields.quantity.handler}
-          />
-        </label>
-        <button onClick={handleSubmit} disabled={!isValid}>
-          Add Item
-        </button>
-      </form>
+      <NewShoppingListItemForm handleNewItemCreation={setListThunks.newItem} />
       <button onClick={setListThunks.withoutLastItem}>Delete Last Item</button>
       <button onClick={setListThunks.clear}>Reset Shopping List</button>
       <ul className="ShoppingList-list">
@@ -111,7 +113,7 @@ function ShoppingList(props) {
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 function App() {
@@ -119,7 +121,7 @@ function App() {
     <div className="main-div">
       <ShoppingList listName="First List for HEB" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
