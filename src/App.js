1a4060558c8d25475441121f6fb28c04fa86c371
Jakie W. Rice <jakie.rice@gmail.com>
2020-12-06 11:06:59 -0600

    chore: push progress for 1:1 session with Jeff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React, { useState } from "react"
-import "./App.css"
+import React, { useState } from 'react'
+import './App.css'
+
+// 4. [X] move list state and form state into ShoppingList component
+// 1. [X] use <form></form> for new item to get keyboard handling
+// 2. [X] fix `handleDelete` functions (splice(-1, 1))
+// 3. [X] refactor `handleDelete` functions to use Array.filter
+// 5. add functionality to delete individual item (need uuid for key gen)
+// 6. create list updater functions
+// 7. create custom hooks for shopping list state and new item form state
+
+const removeLastItemFromList = (list) =>
+  list.filter((_, index) => index !== list.length - 1)
 
 function ShoppingList(props) {
+  const [list, setList] = useState([])
+
+  const [itemName, setItemName] = useState('')
+
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    setList((previousListState) => previousListState.concat(itemName))
+    setItemName('')
+  }
+
   return (
     <div>
       <h1>{props.listName}</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          New Item Name
+          <input
+            type="text"
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+          />
+        </label>
+        <button onClick={handleSubmit}>Add Item</button>
+      </form>
+      <button onClick={() => setList(removeLastItemFromList)}>Delete Last Item</button>
       <ul className="ShoppingList-list">
-        {props.list.map((listItem) => {
+        {list.map((listItem) => {
           return (
             <li key={listItem} className="ShoppingList-item">
               {listItem}
@@ -19,58 +54,9 @@ function ShoppingList(props) {
 }
 
 function App() {
-  const [list, setList] = React.useState([]);
-  const [itemName, setItemName] = React.useState("");
-  const [list1, setList1] = useState([]);
-  const [itemName1, setItemName1]= useState("");
-
-  function handleAddItem() {
-    setList((previousListState) => previousListState.concat(itemName))
-    setItemName("")
-  }
-
-  function handleAddItem1() {
-    setList1((previousListState) => previousListState.concat(itemName1))
-    setItemName1("")
-  }
-
-  function handleDelete(i) {
-   const newList = [...list]
-   newList.splice(i, 1)
-   setList(newList)
-  }
-
-  function handleDelete1(i) {
-    const newList1 = [...list1]
-    newList1.splice(i, 1)
-    setList1(newList1)
-   }
-
   return (
     <div className="main-div">
-      <ShoppingList
-        listName="First List for HEB" list={list1} />
-        <label>
-        New Item Name
-        <input
-          type="text"
-          value={itemName1}
-          onChange={(e) => setItemName1(e.target.value.toUpperCase())}
-        />
-        </label>
-        <button onClick={handleAddItem1}>Add Item</button>
-        <button onClick={handleDelete1}>Delete Item</button>
-      <ShoppingList listName="Second List for Whole Foods" list={list} />
-      <label>
-        New Item Name
-        <input
-          type="text"
-          value={itemName}
-          onChange={(e) => setItemName(e.target.value.toUpperCase())}
-        />
-      </label>
-      <button onClick={handleAddItem}>Add Item</button>
-      <button onClick={handleDelete}>Delete Item</button>
+      <ShoppingList listName="First List for HEB" />
     </div>
   )
 }
