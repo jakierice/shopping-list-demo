a4dab90f7d55c7711e420e55ab207cfdd4b8f209
Jakie W. Rice <jakie.rice@gmail.com>
2020-12-05 20:16:10 -0600

    refactor: modulize ShoppingList app

---
+++ src/ShoppingList.js
@@ -0,0 +1,74 @@
+import React from "react";
+import * as R from 'ramda'
+import { v4 as uuid } from "uuid";
+
+import {  useForm, useList } from "./hooks";
+
+const newItemFormValidations = [
+  R.propSatisfies(R.compose(R.not, R.isEmpty), "name"),
+  R.propSatisfies(R.lt(0), "quantity"),
+];
+
+const newItemFormInitialValues = { name: "", quantity: 0 };
+
+export function NewShoppingListItemForm({ onNewItemSubmit }) {
+  const form = useForm({
+    initialValues: newItemFormInitialValues,
+    validations: newItemFormValidations,
+    onSubmit: R.pipe(R.merge({ key: uuid() }), onNewItemSubmit),
+  });
+
+  return (
+    <form onSubmit={form.handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          value={form.values.name}
+          onChange={form.handleOnChange("name")}
+        />
+      </label>
+      <label>
+        Quantity
+        <input
+          type="number"
+          value={form.values.quantity}
+          onChange={form.handleOnChange("quantity")}
+        />
+      </label>
+      <button onClick={form.handleSubmit} disabled={!form.isValid}>
+        Add Item
+      </button>
+    </form>
+  );
+}
+
+export function ShoppingList(props) {
+  const [shoppingList, setListThunks] = useList([]);
+  return (
+    <div>
+      <h2>{props.listName}</h2>
+      <span>Use the form below to add a new item to your shopping list.</span>
+      <h3>Add New Item</h3>
+      <NewShoppingListItemForm onNewItemSubmit={setListThunks.newItem} />
+      <button onClick={setListThunks.withoutLastItem}>Delete Last Item</button>
+      <button onClick={setListThunks.clear}>Reset Shopping List</button>
+      <ul className="ShoppingList-list">
+        {shoppingList.map((item) => (
+          <li key={item.key} className="ShoppingList-item">
+            <button
+              className="ShoppingList-item-deleteButton"
+              onClick={setListThunks.withoutItem(item.key)}
+            >
+              X
+            </button>
+            <span className="ShoppingList-item-name">
+              {item.name} x {item.quantity}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
